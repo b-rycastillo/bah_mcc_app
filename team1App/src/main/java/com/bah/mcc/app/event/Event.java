6be497f659c8c7002315f323ef2115febb5c2b0a
user <user@this.com>
2022-04-12 11:11:17 -0400

    added event class and eventapi

---
+++ team1App/src/main/java/com/bah/mcc/app/event/Event.java
@@ -0,0 +1,62 @@
+package com.bah.mcc.app.event;
+
+public class Event {
+	
+	long id;
+	
+	String code;
+	
+	String title;
+	
+	String description;
+	
+	public Event() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+	public Event(long id, String code, String title, String description) {
+		super();
+		this.id = id;
+		this.code = code;
+		this.title = title;
+		this.description = description;
+	}
+
+
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	
+}
