1fcb6e315105ac7e6a7f401428084bff6fcd935e
user <user@this.com>
2022-04-13 13:45:52 -0400

    added column names and ids to event.java

--- team1App/src/main/java/com/bah/mcc/app/domain/Event.java
+++ team1App/src/main/java/com/bah/mcc/app/domain/Event.java
@@ -1,16 +1,77 @@
 package com.bah.mcc.app.domain;
 
-//import javax.persistence.Column;
-//import javax.persistence.Entity;
-//import javax.persistence.GeneratedValue;
-//import javax.persistence.GenerationType;
-//import javax.persistence.Id;
-//import javax.persistence.Table;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GenerationType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 //
-//@Entity
-//@Table(name="EVENTS")
+@Entity
+@Table(name="EVENTS")
 public class Event {
-	//  Workshop:
-	//
-	//  Create an Event class, and implement methods to manage event data.
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	long id;
+	
+	@Column(name="CODE")
+	String code;
+	
+	@Column(name="TITLE")
+	String title;
+	
+	@Column(name="DESCRIPTION")
+	String description;
+	
+	public Event() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+	public Event(long id, String code, String title, String description) {
+		super();
+		this.id = id;
+		this.code = code;
+		this.title = title;
+		this.description = description;
+	}
+
+
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	
 }
